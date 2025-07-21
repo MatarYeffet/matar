474aa5a754a211733c1e42745ddad486104bfbcb
MatarYeffet <matar.yeffet@gmail.com>
2025-07-21 15:13:10 +0300

    'autosave'

--- ייצוג ופעולות בינאריות.py
+++
@@ -1,7 +0,0 @@
-def decimal_to_twos_complement(num, bits):
-    if num >= 2**(bits - 1):
-        raise ValueError("המספר גדול מדי לגודל הזיכרון שנבחר")
-    twos_complement = (1 << bits) - num
-    return format(twos_complement, f'0{bits}b')
-
-
