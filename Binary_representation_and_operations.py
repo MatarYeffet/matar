7e93c3dc9411c1608cd642c4ce286d0e40741d80
MatarYeffet <matar.yeffet@gmail.com>
2025-07-21 16:53:51 +0300

    'autosave'

--- Binary_representation_and_operations.py
+++ Binary_representation_and_operations.py
@@ -1,6 +1,6 @@
 def Binary_representation_and_operations(num):
     binary_num = bin(num)[2:]
     for i in range(len(binary_num)):
-        
+        if 
 
 Binary_representation_and_operations(10)
