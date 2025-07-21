e87379467c2d9b82302d118f8c273974f3560da6
MatarYeffet <matar.yeffet@gmail.com>
2025-07-21 17:16:46 +0300

    'autosave'

--- Binary_representation_and_operations.py
+++ Binary_representation_and_operations.py
@@ -1,13 +1,11 @@
 def Binary_representation_and_operations(num, bits=8):
     binary_num = bin(num)[2:].zfill(bits)
-    print(f"Positive binary ({bits} bits): {binary_num}")
     binary_num_list = list(binary_num)
     for i in range(len(binary_num_list)):
         if binary_num_list[i] == '0':
             binary_num_list[i] = '1'
         else:
             binary_num_list[i] = '0'
-    print(f"One's complement: {''.join(binary_num_list)}")
     carry = 1
     for i in range(len(binary_num_list) - 1, -1, -1):
         if binary_num_list[i] == '0' and carry == 1:
@@ -20,8 +18,6 @@ def Binary_representation_and_operations(num, bits=8):
         else:
             break
     negative_binary = ''.join(binary_num_list)
-    print(f"Two's complement (negative): {negative_binary}")
     return negative_binary
-
 
 Binary_representation_and_operations(1,bits=8)
