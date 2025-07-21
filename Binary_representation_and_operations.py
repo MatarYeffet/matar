3b928f114f2193213a5a377a3b6561c3feec868a
MatarYeffet <matar.yeffet@gmail.com>
2025-07-21 17:15:00 +0300

    'autosave'

--- Binary_representation_and_operations.py
+++ Binary_representation_and_operations.py
@@ -1,21 +1,27 @@
-def Binary_representation_and_operations(num):
-    g = 0
-    binary_num = bin(num)[2:]
+def Binary_representation_and_operations(num, bits=8):
+    binary_num = bin(num)[2:].zfill(bits)
+    print(f"Positive binary ({bits} bits): {binary_num}")
     binary_num_list = list(binary_num)
     for i in range(len(binary_num_list)):
-        if binary_num_list[i] == 0:
-            binary_num_list[i] = 1
+        if binary_num_list[i] == '0':
+            binary_num_list[i] = '1'
         else:
-            binary_num_list[i] = 0
-    while binary_num_list[g] != binary_num_list[-1]:
-        if binary_num_list[g] == 0:
-            binary_num_list[g] = 1
+            binary_num_list[i] = '0'
+    print(f"One's complement: {''.join(binary_num_list)}")
+    carry = 1
+    for i in range(len(binary_num_list) - 1, -1, -1):
+        if binary_num_list[i] == '0' and carry == 1:
+            binary_num_list[i] = '1'
+            carry = 0
             break
+        elif binary_num_list[i] == '1' and carry == 1:
+            binary_num_list[i] = '0'
+            carry = 1
         else:
-            binary_num_list[g] = 0
-            g += 1
-    binary_representation_of_a_negative_number = ''.join(str(bit) for bit in binary_num_list)
-    print(binary_representation_of_a_negative_number)
+            break
+    negative_binary = ''.join(binary_num_list)
+    print(f"Two's complement (negative): {negative_binary}")
+    return negative_binary
 
 
-Binary_representation_and_operations(1)
+Binary_representation_and_operations(1,bits=8)
