25acaa3ef98a525125b08311a3963c7452d25365
MatarYeffet <matar.yeffet@gmail.com>
2025-07-21 16:46:32 +0300

    'autosave'

--- Binary_representation_and_operations.py
+++ Binary_representation_and_operations.py
@@ -2,4 +2,4 @@ def Binary_representation_and_operations(num):
     binary_num = bin(num)
     print(binary_num)
 
-Binary_representation_and_operations("10")
+Binary_representation_and_operations(10)
